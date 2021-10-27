c1d2b56af9afe5d415206049bb09ad0d3a409118
Zhuravlev Maksim <maks723@yandex.ru>
2021-10-27 18:51:19 +0300

    init

---
+++ desktop/src/ru/tryouts/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.tryouts.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.tryouts.game.GdxGameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGameMain(), config);
+	}
+}
